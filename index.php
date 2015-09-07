a6ac74bc785ab7c3e5d708c19c55e84014590f90
kineks <bogdan.bele@yahoo.com>
2015-09-07 09:41:37 +0200

    Added index.php
    The new memes are super fresh on this one guys

---
+++ index.php
@@ -0,0 +1,50 @@
+<?php
+get_header();
+
+?>
+<!-- content area -->    
+	<section id="content">
+            <div class="single-post">
+                <h1>Pliny The Elder</h1>
+                <h2 class="category">
+                    <a href="">Havn't had that</a>
+                </h2>
+                <div class="image">
+                    <a href="one_beer.html"><img src="<?php bloginfo('stylesheet_directory');?>/images/bigbender.png"></a>
+                </div>
+                <div class="content">
+                    <p>Boo Hoo, havn't had that one</p>
+                    <p>Nearly impossible to get in San Fransisco, worse in Denmark</p>
+                    <p>Way more text goes here</p>
+                </div>
+                
+                <footer class="">
+                    <div class="tags">
+                        <ul>
+                            <li><a href='#'>Hoppy</a></li>
+                            <li><a href='#'>Dark</a></li>
+                            <li><a href='#'>Malty</a></li>
+                            <li><a href='#'>Spicy</a></li>
+                        </ul>
+                    </div>
+                    
+                </footer>
+             </div>                 
+
+    
+                
+
+<!-- #end content area -->
+      
+      
+    <!-- sidebar -->   
+    
+</section>
+
+ 
+
+<?php
+get_sidebar();
+get_footer();
+
+?>
